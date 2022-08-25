21003148729c1c8e23a32c7f5e98b8fbd51a4e55
edumoreira1506 <contato@eduardoem.com.br>
2022-08-25 20:26:24 -0300

    Add sites list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SiteTitleComponent } from './home/site-title/site-title.component';
+import { SitesListComponent } from './home/sites-list/sites-list.component';
+import { SiteItemComponent } from './home/site-item/site-item.component';
 
 @NgModule({
-  declarations: [AppComponent, SiteTitleComponent],
+  declarations: [AppComponent, SiteTitleComponent, SitesListComponent, SiteItemComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
