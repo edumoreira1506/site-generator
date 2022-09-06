9833b58074e7fef911ef8d5449bcbe785da2e333
edumoreira1506 <contato@eduardoem.com.br>
2022-09-06 20:45:12 -0300

    Add materialize header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import * as Materialize from 'materialize-css';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  @ViewChild('mobile_sidebar') mobileSidebarElement?: ElementRef;
+
   title = 'site-generator';
+
+  ngAfterViewInit() {
+    Materialize.Sidenav.init(this.mobileSidebarElement?.nativeElement);
+  }
 }
