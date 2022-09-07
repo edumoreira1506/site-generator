718852c54251b7867e6b0bb13e7621abe66d51fa
edumoreira1506 <contato@eduardoem.com.br>
2022-09-06 22:16:42 -0300

    Add create site button to home page

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { RoutePaths } from '../app-routing.module';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.scss']
+  styleUrls: ['./home-page.component.scss'],
 })
 export class HomePageComponent implements OnInit {
+  createSiteRoute: string = RoutePaths.CREATE_PAGE;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
